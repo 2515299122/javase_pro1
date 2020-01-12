8fcd3f1441a4810765e73b159695535bf8a3448d
wubing <2515299122@qq.com>
2020-01-12 10:41:25 +0800

    添加数据库表sql文件

---
+++ wubing/src/main/myproject1.sql
@@ -0,0 +1,70 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 5.7.22-log : Database - myproject1
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`myproject1` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `myproject1`;
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `username` varchar(16) DEFAULT NULL,
+  `password` varchar(16) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `department` */
+
+DROP TABLE IF EXISTS `department`;
+
+CREATE TABLE `department` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `logging` */
+
+DROP TABLE IF EXISTS `logging`;
+
+CREATE TABLE `logging` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `user` varchar(16) DEFAULT NULL,
+  `method` varchar(32) DEFAULT NULL,
+  `date` varchar(32) DEFAULT NULL,
+  `methodTime` int(16) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `staff` */
+
+DROP TABLE IF EXISTS `staff`;
+
+CREATE TABLE `staff` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `username` varchar(16) DEFAULT NULL,
+  `password` varchar(16) DEFAULT NULL,
+  `name` varchar(16) DEFAULT NULL,
+  `salary` double(8,2) DEFAULT NULL,
+  `Did` int(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
